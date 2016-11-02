2abcff803895562c9a88a196ba654ceaa28688d0
czii <chenharder@163.com>
2016-11-02 11:13:49 +0800

    v4.4

--- tdget.py
+++ tdget.py
@@ -13,11 +13,36 @@ while 1:
 	try :
 		html = geturl("http://m.91jin.com/hq/lists.html")
 		现货银 = re.search('现货银([\s\S]{200})',html.decode("utf8"),flags=0).group()
+		伦敦金 = re.search('伦敦金([\s\S]{200})',html.decode("utf8"),flags=0).group()
+		伦敦银 = re.search('伦敦银([\s\S]{200})',html.decode("utf8"),flags=0).group()
+
 		现货银 = re.findall('\d\d\d\d',现货银)
+		伦敦金 = re.findall('\d+\.\d+',伦敦金)
+		伦敦银 = re.findall('\d+\.\d+',伦敦银)
+
 		现货银 = (int(现货银[0])+int(现货银[1]))/2
+		伦敦金 = (float(伦敦金[0])+float(伦敦金[1]))/2
+		伦敦银 = (float(伦敦银[0])+float(伦敦银[1]))/2
 	except :
 		print("91jin网络异常")
 		time.sleep(0.01)
+		try :
+			html3 = geturl("http://hq.sinajs.cn/?_=1477967082802/&list=hf_XAU")
+			伦敦金 = re.search('XAU([\s\S]{20})',html3.decode("gbk"),flags=0).group()
+			伦敦金 = re.findall('\d+\.\d+',伦敦金)
+			伦敦金 = float(伦敦金[0])
+		except :
+			print("sina_XAU异常")
+			time.sleep(0.01)
+		try :
+			html4 = geturl("http://hq.sinajs.cn/?_=1477967353119/&list=hf_XAG")
+			伦敦银 = re.search('XAG([\s\S]{20})',html4.decode("gbk"),flags=0).group()
+			伦敦银 = re.findall('\d+\.\d+',伦敦银)
+			伦敦银 = float(伦敦银[0])
+		except :
+			print("sina_XAG异常")
+			time.sleep(0.01)
+
 	try :
 		html2 = geturl("http://hq.sinajs.cn/list=hf_AGTD")
 		白银延期 = re.search('AGTD([\s\S]{10})',html2.decode("gbk"),flags=0).group()
@@ -26,22 +51,7 @@ while 1:
 	except :
 		print("sina_AGTD网络异常")
 		time.sleep(0.01)
-	try :
-		html3 = geturl("http://hq.sinajs.cn/?_=1477967082802/&list=hf_XAU")
-		伦敦金 = re.search('XAU([\s\S]{20})',html3.decode("gbk"),flags=0).group()
-		伦敦金 = re.findall('\d+\.\d+',伦敦金)
-		伦敦金 = float(伦敦金[0])
-	except :
-		print("sina_XAU异常")
-		time.sleep(0.01)
-	try :
-		html4 = geturl("http://hq.sinajs.cn/?_=1477967353119/&list=hf_XAG")
-		伦敦银 = re.search('XAG([\s\S]{20})',html4.decode("gbk"),flags=0).group()
-		伦敦银 = re.findall('\d+\.\d+',伦敦银)
-		伦敦银 = float(伦敦银[0])
-	except :
-		print("sina_XAG异常")
-		time.sleep(0.01)
+
 	try :
 		html5 = geturl("http://hq.sinajs.cn/rn=1477903719956list=DINIW")
 		美元指数 = re.search('DINIW([\s\S]{40})',html5.decode("gbk"),flags=0).group()
@@ -50,7 +60,8 @@ while 1:
 	except :
 		print("sina_USD异常")
 		time.sleep(0.01)
-		#输出部分
+
+	#输出部分
 	#print(time.strftime('%H:%M',time.localtime(time.time())))
 	try :
 		print("白银延期 =","%.0f"%白银延期)
